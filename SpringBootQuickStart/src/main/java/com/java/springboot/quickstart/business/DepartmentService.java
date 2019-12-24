1e0e1a8d7943ccb69e150aa325834156f9273c03
AshokKumarChoppadandi <ashokkumar98778@gmail.com>
2019-12-24 19:03:40 +0530

    Department Service and Controller added

--- SpringBootQuickStart/src/main/java/com/java/springboot/quickstart/business/DepartmentService.java
+++ SpringBootQuickStart/src/main/java/com/java/springboot/quickstart/business/DepartmentService.java
@@ -3,16 +3,17 @@ package com.java.springboot.quickstart.business;
 import com.java.springboot.quickstart.dept.Department;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class DepartmentService {
-    List<Department> departments = Arrays.asList(
+    List<Department> departments = new ArrayList<>(Arrays.asList(
             new Department(901, "CSE", "Computer Science and Engineering"),
             new Department(902, "IT", "Information Technology"),
             new Department(903, "ECE", "Electronics And Communication Engineering")
-    );
+    ));
 
     public List<Department> getAllDepartments() {
         return departments;
@@ -24,5 +25,38 @@ public class DepartmentService {
 
     public void addDepartment(Department department) {
         departments.add(department);
+    }
+
+    public void updateDepartmentDetails(int departmentId, Department department) {
+        for (int i = 0; i < getDepartmentSize(); i++) {
+            Department department1 = departments.get(i);
+            if(department1.getdId() == departmentId) {
+                System.out.println("Inside Update Condition...!!!");
+                departments.set(i, department);
+                return;
+            }
+        }
+    }
+
+    public List<Department> deleteDepartment(int departmentId) {
+        /*
+        for (int i = 0; i < getDepartmentSize() - 1; i++) {
+            Department department = departments.get(i);
+            if(department.getdId() == departmentId) {
+                System.out.println("Inside Delete Condition");
+                departments.remove(i);
+                break;
+            }
+        }
+        return departments;
+        */
+
+        // Using Java 8 functionality
+        departments.removeIf(x -> x.getdId() == departmentId);
+        return departments;
+    }
+
+    private int getDepartmentSize() {
+        return departments.size();
     }
 }
--- SpringBootQuickStart/src/main/java/com/java/springboot/quickstart/controller/DepartmentController.java
+++ SpringBootQuickStart/src/main/java/com/java/springboot/quickstart/controller/DepartmentController.java
@@ -3,10 +3,7 @@ package com.java.springboot.quickstart.controller;
 import com.java.springboot.quickstart.business.DepartmentService;
 import com.java.springboot.quickstart.dept.Department;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,7 +22,8 @@ public class DepartmentController {
      * This Endpoint return all the topics as a List, which creates a new List every time when the REST endpoint is called.
      * @return : List of Departments
      */
-    @RequestMapping("/static_departments1")
+    //@RequestMapping("/static_departments1")
+    @GetMapping(value = "/static_departments1")
     public List<Department> getAllStaticDepartments() {
         return Arrays.asList(
                 new Department(101, "CSE", "Computer Science and Engineering"),
@@ -39,7 +37,8 @@ public class DepartmentController {
      * The Departments List is initialized on only and return the same List whenever the REST endpoint is called.
      * @return : List of Departments
      */
-    @RequestMapping("/static_departments2")
+    //@RequestMapping("/static_departments2")
+    @GetMapping(value = "/static_departments2")
     public List<Department> getAllStaticDepartments2() {
         return departmentService.getAllDepartments();
     }
@@ -47,18 +46,49 @@ public class DepartmentController {
     /**
      * This returns a Department details when the Department ID is passed as an argument.
      * Note: {id} - This is the PathVariable, ideally the name of the Path Variable and method Argument name should be same.
+     * Here name = "id" is used to map the Path Variable to "departmentId"
+     * Here the Argument name is used "departmentId"
      * @param departmentId : Integer
      * @return : Department
      */
-    @RequestMapping("/static_department/{id}") // Here 'id' is used as a Path Variable
-    public Department getStaticDepartmentById(@PathVariable(name = "id") /*Here name = "id" is used to map the Path Variable to "departmentId"*/ int departmentId /* Here the Argument name is used "departmentId"*/) {
+    //@RequestMapping("/static_department/{id}") // Here 'id' is used as a Path Variable
+    @GetMapping(value = "/static_departments2/{id}")
+    public Department getStaticDepartmentById(@PathVariable(name = "id") int departmentId) {
         return departmentService.getDepartmentDetailsById(departmentId);
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/departments")
-    public void addDepartment(Department department) {
+    /**
+     * This REST endpoint takes the Department as an argument from the Request Body and adds to the Departments list.
+     * @param department : Department
+     */
+    //@RequestMapping(method = RequestMethod.POST, value = "/add_department")
+    @PostMapping("/add_department")
+    public void addDepartment(@RequestBody Department department) {
+        System.out.println("DEPARTMENT :: " + department);
         departmentService.addDepartment(department);
     }
 
+    /**
+     * This REST endpoint updates a Department details, it takes two arguments.
+     *   1. The Department ID which need to be updated.
+     *   2. The Department object - New Department Object after updating.
+     * @param departmentId : Integer
+     * @param department : Department
+     */
+    //@RequestMapping(method = RequestMethod.PUT, value = "/update_department/{id}")
+    @PutMapping(value = "/update_department/{id}")
+    public void updateDepartment(@PathVariable(name = "id") int departmentId, @RequestBody Department department) {
+        departmentService.updateDepartmentDetails(departmentId, department);
+    }
 
+
+    /**
+     * This REST endpoint delete a Department from the List when a Department ID is passed as an argument.
+     * @param departmentId : Integer
+     * @return : Returns List of Departments
+     */
+    @DeleteMapping("/delete_department/{id}")
+    public List<Department> deleteDepartment(@PathVariable(name = "id") int departmentId) {
+        return departmentService.deleteDepartment(departmentId);
+    }
 }
